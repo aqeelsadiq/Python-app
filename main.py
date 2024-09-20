4df76865486b4c10702bfe09e8caa4ff7cab8144
nabeel <nabeel@gmail.com>
2024-09-20 11:47:22 +0500

    this is cicd check

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ app = Flask(__name__)
 @app.route('/')
 def hello_world():
  #   now = datetime.now()
-    return f"Hello, World!This is my python application"
+    return f"Hello, World!This is my python application and i deploy it on aws ecs using cicd pipeline"
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=5000)
 
